becbac0d4c22ce6e2b4b9bd7cdc31909a5a3b8bc
Praveennaik6 <praveennaik0814@gmail.com>
2025-10-29 19:59:38 +0530

    adding code to template file

--- template.py
+++ template.py
@@ -13,3 +13,18 @@ list_of_files = [
 "app.py",
 "research/trials.ipynb"
 ]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as fp:
+            pass
+        logging.info(f"Creating file: {filepath}")
+    
